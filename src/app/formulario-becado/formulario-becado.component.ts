5b98d5dda9d5248f0106d7c43d65c2a04ab4669e
albino <xalbinox2@gmail.com>
2021-09-10 10:35:48 -0400

    formulario becado

---
+++ src/app/formulario-becado/formulario-becado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-becado',
+  templateUrl: './formulario-becado.component.html',
+  styleUrls: ['./formulario-becado.component.sass']
+})
+export class FormularioBecadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
