d4c21cbfe7e7fe16bd3f1913cf921118b604d62e
vrinox <vrinox@hotmail.com>
2021-09-11 16:12:01 +0200

    cambio de api

--- src/app/scholar-data.service.ts
+++ src/app/scholar-data.service.ts
@@ -1,23 +1,50 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { earningsData, scholarOfficialData, statsData } from './models/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScholarDataService {
-  private REST_API_SERVER = 'https://api.lunaciarover.com/stats';
+  private REST_API_SERVER = 'https://api.lunaciaproxy.cloud';
 
   constructor(private httpClient: HttpClient) { }
 
-  public get(roninAddres: string):Observable<any>{
+  public async get(roninAddres: string):Promise<any>{
+    let earnings, stats;
+    [stats, earnings] = await Promise.all(
+      [this.getStats(roninAddres),
+      this.getEarnings(roninAddres)]
+    );
+    let apiData: scholarOfficialData = this.parseData(earnings, stats, roninAddres);
+    return apiData;
+  }
+  private getStats(roninAddres: string):Promise<any>{
     return this.httpClient
-      .get(`${this.REST_API_SERVER}/${roninAddres}`)
-      .pipe(
-        map((response: any) => {
-          return response;
-        })
-      )
+      .get(`${this.REST_API_SERVER}/_stats/${roninAddres}`)
+      .toPromise()
+      .then((statsData:any)=>{
+        return statsData.stats;
+      })
+  }
+  private getEarnings(roninAddres: string):Promise<any>{
+    return this.httpClient
+      .get(`${this.REST_API_SERVER}/_earnings/${roninAddres}`)
+      .toPromise()
+      .then((earningsData:any)=>{
+        return earningsData.earnings;
+      })
+  }
+  private parseData(earnings: earningsData, stats: statsData, roninAddress: string){
+    return {
+      ronin_address: roninAddress,
+      ronin_slp: earnings.slp_holdings,
+      total_slp: earnings.slp_in_total,
+      in_game_slp: earnings.slp_inventory,
+      rank: stats.rank,
+      mmr: stats.elo,
+      total_matches: stats.win_total,
+      ign: stats.name
+    }
   }
 }
