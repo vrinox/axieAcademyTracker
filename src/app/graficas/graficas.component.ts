a0bda67a74576613300c480a23e5fe49860483fc
albino <xalbinox2@gmail.com>
2021-09-27 16:44:12 -0400

    me cago en todo

--- src/app/graficas/graficas.component.ts
+++ src/app/graficas/graficas.component.ts
@@ -1,11 +1,11 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { HistoricService } from '../services/historic/historic.service';
 import { Scholar } from 'src/app/models/scholar';
 import { historic } from '../models/historic';
 import { HistoricData } from '../models/interfaces';
 import { ActivatedRoute, Params } from '@angular/router';
 import Chart from 'chart.js/auto';
-import { FormControl } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';;
 import { map, startWith } from 'rxjs/operators';
 import { ReferenceScholarsService } from '../services/referenceScholars/reference-scholars.service';
@@ -18,14 +18,19 @@ import { ReferenceScholarsService } from '../services/referenceScholars/referenc
 })
 export class GraficasComponent implements OnInit {
   @ViewChild('ctx', { static: true }) Ctx?: ElementRef;
+  @ViewChildren('inputUser') InputUser!: QueryList<ElementRef>
+
   canvas: any = [];
-  myControl = new FormControl();
-  myControl2 = new FormControl();
+
+  formNameUser = this.formBuilder.group({
+    nameScholar: new FormArray([])
+  });
+
   dateStart = new FormControl();
   dateEnd = new FormControl();
-  options: string[] = [];
+  options: string[] = ['one', 'two', 'thress'];
   filteredOptions: Observable<string[]>;
-  filteredOptions2: Observable<string[]>;
+
   dateHide: number = 2;
 
   addInput: boolean = false;
@@ -33,26 +38,33 @@ export class GraficasComponent implements OnInit {
   constructor(
     private historic: HistoricService,
     private router: ActivatedRoute,
-    private referenceScholars:  ReferenceScholarsService
+    private referenceScholars:  ReferenceScholarsService,
+    private formBuilder: FormBuilder
     ) { 
       this.filteredOptions = new Observable();
-      this.filteredOptions2 = new Observable();
     }
 
   ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges
-      .pipe(
-        startWith(''),
-        map((value: string) => this._filter(value))
-    );
-    this.filteredOptions2 = this.myControl2.valueChanges
-      .pipe(
-        startWith(''),
-        map((value: string) => this._filter(value))
-    );
+    // this.filteredOptions = this.formNameUser.controls.input_1.valueChanges
+    //   .pipe(
+    //     startWith(''),
+    //     map(value => this._filter(value))
+    //   );
+    this.addNewControlName();
+    Object.keys(this.nameScholar.controls).forEach(e=>{
+      
+    })
+  }
 
-    this.startHistoric();
-    this.getScholars();
+  get nameScholar(): FormArray{
+    return this.formNameUser.get('nameScholar') as FormArray;
+  }
+
+  addNewControlName(){
+    const lessonForm = this.formBuilder.group({
+      inputScholar: new FormControl(''),
+    });
+    this.nameScholar.push(lessonForm);
   }
 
   openCloseMenu(close: number){
@@ -65,69 +77,17 @@ export class GraficasComponent implements OnInit {
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
-  private getScholars(){
-    this.referenceScholars.scholar.forEach((scholar: Scholar, index) => {
-      this.options.push(`${index + 1 }- ${scholar.name}`);
-    });
+  test(){
+    console.log(this.formNameUser.controls.input_1.value);
   }
 
   private startHistoric(): void{
     let ronin_address: string = this.router.snapshot.params['roninAddress'];
     if(ronin_address === 'months'){
-      this.getMontScholars();
+
     }else{
-      this.getHistoricOneScholar(ronin_address);
-    }
-  }
 
-  getHistoricOneScholar(roninAddress: string): void{
-    this.historic.getHistoric(roninAddress)
-    .then((scholar: Scholar[])=>{
-      const historicData = historic.getDataOneScholarHisctoric(scholar);
-      this.createHistoric(historicData);
-    })
-    this.myControl.setValue('');
-    this.myControl2.setValue('');
-  }
-
-  getMontScholars(): void{
-    let first: Date = new Date(new Date().getFullYear(), new Date().getMonth());
-    let last: Date = new Date(first.getFullYear(), first.getMonth() + 1, first.getDate());
-    if(this.dateStart.value != null && this.dateEnd.value != null){
-      first = this.dateStart.value;
-      last = this.dateEnd.value;
     }
-    this.historic.getMontHistoric(first, last).then((scholar: Scholar[])=>{
-      const historicData: HistoricData = historic.getTwoMonthHistoric(scholar, first, last);
-      this.createHistoric(historicData);
-    });
-    this.dateStart.setValue('');
-    this.dateEnd.setValue('');
-  }
-
-  compareScholarHistoric(): void{
-    if(this.myControl.value != null && this.myControl2.value != null){
-      this.historic.getTowScholars(this.referenceScholars.scholar[parseInt(this.myControl.value) - 1].roninAddress, 
-      this.referenceScholars.scholar[parseInt(this.myControl2.value) - 1 ].roninAddress)
-      .then((scholar: Scholar[])=>{
-        const historicData: HistoricData =  historic.compareTwoScholar(scholar, scholar[0].roninAddress);
-        let dataset = {
-            label: historicData.historic?.title,
-            data: historicData.historic?.slp,
-            fill: true,
-            backgroundColor: '#3431c9a6',
-            borderColor: '#3431c9',
-            borderWidth: 1
-        }
-        this.createHistoric(historicData, dataset);
-      })
-    }else if(this.myControl.value != null){
-      this.getHistoricOneScholar(this.referenceScholars.scholar[parseInt(this.myControl.value)-1].roninAddress)
-    }else if(this.myControl2.value != null){
-      this.getHistoricOneScholar(this.referenceScholars.scholar[parseInt(this.myControl.value)-1].roninAddress)
-    }
-    this.myControl.setValue('');
-    this.myControl2.setValue('');
   }
 
   private createHistoric(
