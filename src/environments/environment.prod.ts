5b3ca58db63eff36b7b7c4341c06c38dabdc0c5e
vrinox <vrinox@hotmail.com>
2021-11-21 16:24:41 +0100

    hosting go

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB4NF-O5Ps2e_cMgcTGcbTXOCTr4YT_Sio",
+    authDomain: "axieacademytracker.firebaseapp.com",
+    databaseURL: "https://axieacademytracker-default-rtdb.firebaseio.com",
+    projectId: "axieacademytracker",
+    storageBucket: "axieacademytracker.appspot.com",
+    messagingSenderId: "337378328440",
+    appId: "1:337378328440:web:e4b18e8b2afc0af62ed28d",
+    measurementId: "G-FP3K84QLWF"
+  }
 };
