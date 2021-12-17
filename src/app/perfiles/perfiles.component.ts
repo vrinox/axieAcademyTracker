ae22728e1c3cec89c3d3d10b214e327bd9ae792a
fierce962 <xalbinox2@gmail.com>
2021-12-16 21:08:10 -0400

    coment claimSLP

--- src/app/perfiles/perfiles.component.ts
+++ src/app/perfiles/perfiles.component.ts
@@ -45,7 +45,6 @@ export class PerfilesComponent implements OnInit {
     this.dark = this.sessions.dark;
     this.changeDarkMode();
     this.start();
-    this.claimSlp();
   }
 
   async start(): Promise<void>{
@@ -138,8 +137,8 @@ export class PerfilesComponent implements OnInit {
   }
 
   async claimSlp(): Promise<void>{
-    secrets.forEach(scholar => {
-      this.autoClaim.startClaimSlp(scholar.ronin, scholar.secret);
-    });
+    // secrets.forEach(scholar => {
+    //   this.autoClaim.startClaimSlp(scholar.ronin, scholar.secret);
+    // });
   }
 }
