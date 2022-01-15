effcd92e5a76de49c28ad018f92ddec59fec217a
fierce962 <xalbinox2@gmail.com>
2022-01-15 18:32:44 -0400

    lading page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,4 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, HostListener, Inject, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,15 +7,12 @@ import { Component, HostListener, Inject, OnInit, Renderer2 } from '@angular/cor
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { }
+  constructor(private renderer: Renderer2) {
+  }
 
   test: string[] = ['dsadas', 'asddsadas', 'dasdsadas', 'dsasdasdas', 'dsadasdas']
-  @HostListener('scroll', ['$event'])
-  onScroll(event: any): void {
-      console.log('I am scrolled', event);
-  }
 
   ngOnInit(): void {
-  }
 
+  }
 }
