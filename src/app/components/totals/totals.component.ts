09cf91cd298e9f554ea96badbd821d1a1005ec9d
vrinox <vrinox@hotmail.com>
2022-01-24 21:42:17 +0100

    este

--- src/app/components/totals/totals.component.ts
+++ src/app/components/totals/totals.component.ts
@@ -14,7 +14,7 @@ export class TotalsComponent implements OnInit {
   historial: Historial[] = [];
   imageSrc: string = 'assets/img/SLP.png';
   imageAlt: string = 'SLP';
-  trofeoImgSrc: string = 'assets/img/trofeo.jpg';
+  trofeoImgSrc: string = 'assets/img/mvp.png';
   trofeoAlt: string = 'copas axie infinity';
   slpPrice: number = 0;
   @Input() scholars: Scholar[] = [];
